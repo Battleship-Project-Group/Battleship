37f79df6e899ea0620b9316482a9ad43f0325824
jesc32 <jesc31@ymail.com>
2020-11-16 16:34:19 -0800

    Basic log-in,sign-up functionality added using database file included

---
+++ src/scripts/login.js
@@ -0,0 +1,26 @@
+var httpRequest;
+
+function login() {
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('pw').value;
+  httpRequest = new XMLHttpRequest();
+  if (!httpRequest) {
+    alert('Cannot create an XMLHTTP instance');
+    return false;
+  }
+  httpRequest.onreadystatechange = function() {
+    try {
+      if (httpRequest.readyState == XMLHttpRequest.DONE) {
+        if (httpRequest.status == 200) {
+          var response = httpRequest.responseText;
+          alert(response);
+        }
+      }
+    } catch (e) {
+      console.log("Caught exception: " + e.description + ", while attempting to log in");
+    }
+  }
+  httpRequest.open('POST','scripts/login.php');
+  httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  httpRequest.send('uname=' + username + '&pw=' + password);
+}
---
+++ src/scripts/signup.js
@@ -0,0 +1,86 @@
+var httpRequest;
+var validEmail = false;
+var validPassword = false;
+var matchingEmail = false;
+var matchingPassword = false;
+
+function validatePassword() {
+  //code this later, for now return true always
+  validPassword = true;
+  canSubmit();
+}
+
+function validateEmail() {
+  //code this later, for now return true always
+  validEmail = true;
+  canSubmit();
+}
+
+function pwMatch() {
+  let pw1 = document.getElementById('password').value;
+  let pw2 = document.getElementById('cpassword').value;
+  if (pw1 == pw2){
+    matchingPassword = true;
+  } else {
+    matchingPassword = false;
+  }
+  canSubmit();
+}
+
+function emailMatch() {
+  let e1 = document.getElementById('email').value;
+  let e2 = document.getElementById('cemail').value;
+  if (e1 == e2){
+    matchingEmail = true;
+  } else {
+    matchingEmail = false;
+  }
+  canSubmit();
+}
+
+function canSubmit() {
+  console.log("canSubmit() call", validEmail, validPassword, matchingEmail, matchingPassword);
+  if (validEmail && validPassword && matchingEmail && matchingPassword){
+    showSubmit();
+    return true;
+  } else {
+    hideSubmit();
+    return false;
+  }
+}
+
+function hideSubmit() {
+  let button = document.getElementById('signup');
+  button.disabled = true;
+}
+
+function showSubmit() {
+  let button = document.getElementById('signup');
+  button.disabled = false;
+}
+
+function submit() {
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+  let email = document.getElementById('email').value;
+  httpRequest = new XMLHttpRequest();
+  if (!httpRequest) {
+    alert('Cannot create an XMLHTTP instance');
+    return false;
+  }
+  httpRequest.onreadystatechange = function() {
+    try {
+      if (httpRequest.readyState == XMLHttpRequest.DONE) {
+        if (httpRequest.status == 200) {
+          var response = httpRequest.responseText;
+          alert(response);
+        }
+      }
+    } catch (e) {
+      console.log("Caught exception: " + e.description + ", while attempting to register");
+    }
+  }
+  httpRequest.open('POST','scripts/signup.php');
+  httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  httpRequest.send('uname=' + username + '&pw=' + password + '&email=' + email);
+}
