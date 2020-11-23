568715e14cd6a1bd4e615abb9b7244a25e38c776
jesc32 <jesc31@ymail.com>
2020-11-22 16:09:12 -0800

    Added functions to join game, create lobby, and ready up

--- src/scripts/initShips.php
+++ src/scripts/initShips.php
@@ -1,18 +1,17 @@
 <?php
 $username = (isset($_POST['uname'])) ? $_POST['uname'] : "test";
 $lobbyname = (isset($_POST['lobbyname'])) ? $_POST['lobbyname'] : $username . "-Lobby";
-$status = (isset($_POST['ready'])) ? $_POST['ready'] : "True";
+$carrier = (isset($_POST['carrier'])) ? $_POST['carrier'] : "A1B1C1D1E1";
+$bship = (isset($_POST['battleship'])) ? $_POST['battleship'] : "B1C1D1E1";
+$destroyer = (isset($_POST['destroyer'])) ? $_POST['destroyer'] : "C1D1E1";
+$submarine = (isset($_POST['submarine'])) ? $_POST['submarine'] : "C1D1E1";
+$patrol = (isset($_POST['patrolboat'])) ? $_POST['patrolboat'] : "D1E1";
 $servername = "localhost";
 $dbusername = "BattleshipProjectUser";
 $dbpass = "shipbattle321";
 $dbname = "gamesdb";
 
-$sql = "UPDATE `" . $lobbyname . "` SET ReadyStatus = ";
-if ($status == "True"){
-  $sql .= "'1'";
-} else {
-  $sql .= "'0'";
-}
+$sql = "UPDATE `" . $lobbyname . "` SET `CarrierLocation`=" . $carrier . ",`BattleshipLocation`=" . $bship . ",`DestroyerLocation`=" . $destroyer . ",`SubmarineLocation`=" . $submarine . ",`PatrolLocation`=" . $patrol;
 $sql .= " WHERE PlayerName = '" . $username . "';";
 
 $conn = new mysqli($servername,$dbusername,$dbpass,$dbname);
