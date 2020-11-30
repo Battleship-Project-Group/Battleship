a392e4b4e1c7fbad24e2c0a1cf205cb7180efac9
Michael Spohn <71857692+MichaelSpohn@users.noreply.github.com>
2020-11-29 21:56:28 -0800

    Setting up for attacks to show as well as sending attack information

--- src/scripts/attack.php
+++ src/scripts/attack.php
@@ -1,13 +1,15 @@
 <?php
 $username = (isset($_POST['uname'])) ? $_POST['uname'] : "test";
 $lobbyname = (isset($_POST['lobbyname'])) ? $_POST['lobbyname'] : $username . "-Lobby";
-$target = (isset($_POST['target'])) ? $_POST['target'] : "A1";
+//$target = (isset($_POST['target'])) ? $_POST['target'] : "A1";
+$targets_str = (isset($_POST['targets'])) ? $_POST['targets'] : "";
+$targets_obj = json_decode($targets);
 $servername = "localhost";
 $dbusername = "BattleshipProjectUser";
 $dbpass = "shipbattle321";
 $dbname = "gamesdb";
 
-$sql = "UPDATE `" . $lobbyname . "` SET `LastTarget`='" . $target . "' WHERE `PlayerName`='" . $username . "';";
+$sql = "UPDATE `" . $lobbyname . "` SET `LastTarget`='" . $targets_obj . "' WHERE `PlayerName`='" . $username . "';";
 
 $conn = new mysqli($servername,$dbusername,$dbpass,$dbname);
 
