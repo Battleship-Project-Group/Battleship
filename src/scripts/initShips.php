b1fb8423dfe249e75bdcb6d9be8534b69ef5917c
Jese Hernandez <jesc31@ymail.com>
2020-12-01 20:20:33 -0800

    updates

--- src/scripts/initShips.php
+++ src/scripts/initShips.php
@@ -9,16 +9,20 @@
 
 $username = (isset($_POST['uname'])) ? $_POST['uname'] : "test";
 $lobbyname = (isset($_POST['lobbyname'])) ? $_POST['lobbyname'] : $username . "-Lobby";
-$carrier = (isset($_POST['carrier'])) ? parseInput($_POST['carrier']) : "E7F7G7H7I7";
-$bship = (isset($_POST['battleship'])) ? parseInput($_POST['battleship']) : "C9D9E9F9";
-$destroyer = (isset($_POST['destroyer'])) ? parseInput($_POST['destroyer']) : "I3I4I5";
-$submarine = (isset($_POST['submarine'])) ? parseInput($_POST['submarine']) : "D3E3F3";
-$patrol = (isset($_POST['patrolboat'])) ? parseInput($_POST['patrolboat']) : "A1B1";
+$carrier = (isset($_POST['carrier'])) ? ($_POST['carrier']) : "E7F7G7H7I7";
+$bship = (isset($_POST['battleship'])) ? ($_POST['battleship']) : "C9D9E9F9";
+$destroyer = (isset($_POST['destroyer'])) ? ($_POST['destroyer']) : "I3I4I5";
+$submarine = (isset($_POST['submarine'])) ? ($_POST['submarine']) : "D3E3F3";
+$patrol = (isset($_POST['patrolboat'])) ? ($_POST['patrolboat']) : "A1B1";
+$all = (isset($_POST['all'])) ? ($_POST['all']) : "";
 $servername = "localhost";
 $dbusername = "BattleshipProjectUser";
 $dbpass = "shipbattle321";
 $dbname = "gamesdb";
 
+echo $username . "   ";
+echo $lobbyname . "   ";
+echo $all . "   ";
 $sql = "UPDATE `" . $lobbyname . "` SET `CarrierLocation`='" . $carrier . "',`BattleshipLocation`='" . $bship . "',`DestroyerLocation`='" . $destroyer . "',`SubmarineLocation`='" . $submarine . "',`PatrolLocation`='" . $patrol . "'";
 $sql .= " WHERE `PlayerName`='" . $username . "';";
 
